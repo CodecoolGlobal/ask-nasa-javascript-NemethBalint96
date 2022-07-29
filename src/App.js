2653a1aff4efb02bc8e414f960e5e3726e032545
NemethBalint96 <nemeth.balint1996@gmail.com>
2022-07-29 07:53:47 +0200

    Create an input for previous days

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Post from './components/Post';
-import { getTodaysPost } from './Fetch';
+import { getPostByDate, getTodaysPost } from './Fetch';
 import React, { useEffect, useState } from 'react';
 
 function App() {
   const [post, setPost] = useState(null);
-  
+  const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
+  const maxDate = new Date().toISOString().slice(0, 10);
+
   useEffect(() => {
     let mounted = true;
     getTodaysPost().then(data => {
@@ -16,9 +18,20 @@ function App() {
     return () => mounted = false;
   }, [])
 
+  function handleChange(e) {
+    setDate(e.currentTarget.value);
+    getPostByDate(e.currentTarget.value).then(data => {
+      setPost(data);
+    })
+  }
+
   return (
     <div className="App">
-      <Post data={post}/>
+      <input type="date"
+        value={date}
+        max={maxDate}
+        onChange={handleChange}></input><br></br>
+      {post && <Post data={post}/>}
     </div>
   );
 }
--- src/Fetch.js
+++ src/Fetch.js
@@ -1,4 +1,9 @@
 export function getTodaysPost() {
-    return fetch('https://api.nasa.gov/planetary/apod?api_key=SiMRkZXqAg7DNjt0A5UYqrRxdbmBBt3uQxvEUBj2')
+  return fetch('https://api.nasa.gov/planetary/apod?api_key=SiMRkZXqAg7DNjt0A5UYqrRxdbmBBt3uQxvEUBj2')
+    .then(response => response.json())
+}
+
+export function getPostByDate(date) {
+  return fetch(`https://api.nasa.gov/planetary/apod?api_key=goKcJW5jRlSawdjoOH0R3dVrMsgf1YWvyyuao9Jt&date=${date}`)
       .then(response => response.json())
-  }
+}
